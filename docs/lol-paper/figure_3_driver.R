0b2b9610919c5bbea1cdf865528cc7ccd82c982f
eric bridgeford <ebridge2@jhu.edu>
2018-01-09 11:42:25 -0500

    updated figure 3 code to have sims created inline

--- docs/lol-paper/figure_3_driver.R
+++ docs/lol-paper/figure_3_driver.R
@@ -25,8 +25,8 @@ counter <- 1
 
 for (i in 1:length(sims)) {
   for (j in 1:niter) {
-    sim_dat <- do.call(sims[[i]], c(list(n, ds[i]), opt_args[[i]]))
-    simulations[[counter]] <- list(X=sim_dat$X, Y=sim_dat$Y, rmax=maxr[i], sim=sim_names[i], iter=j)
+    simulations[[counter]] <- list(sim_func=sims[[i]], args=c(list(n, ds[i]), opt_args[[i]]),
+                                   rmax=maxr[i], sim=sim_names[i], iter=j)
     counter <- counter + 1
   }
 }
@@ -40,12 +40,14 @@ clusterExport(cl, "algs"); clusterExport(cl, "alg_name"); clusterExport(cl, "sim
 clusterExport(cl, "rs"); clusterExport(cl, "maxr")
 results <- lapply(simulations, function(sim) {
   require(fselect)
+  sim_dat <- do.call(sim$sim_func, sim$args)
+  X <- sim_dat$X; Y <- sim_dat$Y
   results <- data.frame(sim=c(), iter=c(), alg=c(), r=c(), lhat=c())
   for (i in 1:length(algs)) {
     rs <- round(seq(from=1, to=sim$rmax, length.out=rlen))
     for (r in rs) {
       tryCatch({
-        xv_res <- fs.xval.eval(sim$X, sim$Y, r, algs[[i]])
+        xv_res <- fs.xval.eval(X, Y, r, algs[[i]])
         lhat <- xv_res$Lhat
       }, error=function(e) lhat <- NaN)
       results <- rbind(results, data.frame(sim=sim$sim, iter=sim$iter, alg=alg_name[i], r=r, lhat=lhat))
