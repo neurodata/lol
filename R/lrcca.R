6b2d14d97b89b9939a17a66232383dddf387c546
Eric Bridgeford <ebridge2@jhu.edu>
2018-01-23 08:31:24 -0500

    updated lrcca

--- R/lrcca.R
+++ R/lrcca.R
@@ -3,15 +3,20 @@
 #' A function for implementing the Low-rank Canonical Correlation Analysis (LR-CCA) Algorithm.
 #'
 #' @import irlba
-#' @param X [n, p] the data with n samples in d dimensions.
+#' @param X [n, d] the data with n samples in d dimensions.
 #' @param Y [n] the labels of the samples.
 #' @param r the rank of the projection.
-#' @return A [d, r] the projection matrix from d to r dimensions.
+#' @param method='full' The method to use for LR-CCA.
+#' \itemize{
+#' \item{'full'}{Requires O(d^2) storage, but is faster.}
+#' \item{'partial'}{Requires O(n^2) storage, but is slower.}
+#' }
+#' @return A [d, r] the projection matrix from d to r dimensions. Only returned if method='full'.
 #' @return ylabs [K] vector containing the unique, ordered class labels.
 #' @return centroids [K, d] centroid matrix of the unique, ordered classes.
 #' @return priors [K] vector containing prior probability for the unique, ordered classes.
 #' @return Xr [n, r] the data in reduced dimensionality.
-#' @return cr [K, r] the centroids in reduced dimensionality.
+#' @return cr [K, r] the centroids in reduced dimensionality. Only returned if method='full'.
 #' @author Jason Yim and Eric Bridgeford
 #' @examples
 #' library(lol)
@@ -19,7 +24,18 @@
 #' X <- data$X; Y <- data$Y
 #' model <- lol.project.lrcca(X=X, Y=Y, r=5)  # use lrcca to project into 5 dimensions
 #' @export
-lol.project.lrcca <- function(X, Y, r, ...) {
+lol.project.lrcca <- function(X, Y, r, method='full', ...) {
+  if (method == 'full') {
+    outputs <- lol.project.full_lrcca(X, Y, r)
+  } else if (method == 'partial') {
+    outputs <- lol.project.partial_lrcca(X, Y, r)
+  } else {
+    stop(sprintf("You have passed an invalid method, %s. Options are 'full' and 'partial'.", method))
+  }
+  return(outputs)
+}
+
+lol.project.full_lrcca <- function(X, Y, r, ...) {
   info <- lol:::lol.utils.info(X, Y)
   priors <- info$priors; centroids <- t(info$centroids)
   K <- info$K; ylabs <- info$ylabs
@@ -41,4 +57,18 @@ lol.project.lrcca <- function(X, Y, r, ...) {
 
   return(list(A=A, centroids=centroids, priors=priors, ylabs=ylabs,
               Xr=X %*% A, cr=centroids %*% A))
+}
+
+lol.project.partial_lrcca <- function(X, Y, r, ...) {
+  info <- lol:::lol.utils.info(X, Y)
+  priors <- info$priors; centroids <- t(info$centroids)
+  K <- info$K; ylabs <- info$ylabs
+  n <- info$n; d <- info$d
+
+  Y <- array(Y, dim=c(n, 1))  # force dimensionality of Y
+  svd_out <- svd(X)  # svd of X
+  vxvxt <- svd_out$u %*% t(svd_out$u)
+  M <- svd(1/n*vxvxt %*% Y %*% t(Y))$u
+  Xr <- M[, 1:r]  # dimension-reduced vectors are top r columns
+  return(list(Xr=Xr, centroids=centroids, priors=priors, ylabs=ylabs))
 }
