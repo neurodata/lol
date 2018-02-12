fc224ccde9296a5b03ee9e69a027a5ee495698d3
Eric Bridgeford <ebridge2@jhu.edu>
2018-02-12 04:56:11 -0500

    added unit, rank, and log scaling options

--- R/pca.R
+++ R/pca.R
@@ -5,6 +5,15 @@
 #' @importFrom irlba irlba
 #' @param X \code{[n, d]} the data with \code{n} samples in \code{d} dimensions.
 #' @param r the rank of the projection.
+#' @param xfm whether to transform the variables before taking the SVD.
+#' \itemize{
+#' \item{FALSE}{apply no transform to the variables.}
+#' \item{'unit'}{unit transform the variables, defaulting to centering and scaling to mean 0, variance 1. See \link[base]{scale} for details and optional args.}
+#' \item{'log'}{log-transform the variables, for use-cases such as having high variance in larger values. Defaults to natural logarithm. See \link[base]{log} for details and optional args.}
+#' \item{'rank'}{rank-transform the variables. Defalts to breaking ties with the average rank of the tied values. See \link[base]{rank} for details and optional args.}
+#' \item{c(opt1, opt2, etc.)}{apply the transform specified in opt1, followed by opt2, etc.}
+#' }
+#' @param xfm.opts optional arguments to pass to the \code{xfm} option specified. Should be a numbered list of lists, where \code{xfm.opts[[i]]} corresponds to the optional arguments for \code{xfm[i]}. Defaults to the default options for each transform scheme.
 #' @param ... trailing args.
 #' @return A list of class \code{embedding} containing the following:
 #' \item{A}{\code{[d, r]} the projection matrix from \code{d} to \code{r} dimensions.}
@@ -17,10 +26,10 @@
 #' X <- data$X; Y <- data$Y
 #' model <- lol.project.pca(X=X, r=2)  # use pca to project into 2 dimensions
 #' @export
-lol.project.pca <- function(X, r, ...) {
-  # mean center by the global mean
+lol.project.pca <- function(X, r, xfm=FALSE, xfm.opts=list(), ...) {
+  # mean center by the column mean
   Xc  <- sweep(X, 2, colMeans(X), '-')
-  svdX <- lol.utils.svd(Xc, nv=r, nu=0)
+  svdX <- lol.utils.svd(Xc, xfm=xfm, scale.opts=xfm.opts, nv=r, nu=0)
 
   return(list(A=svdX$v, d=svdX$d, Xr=lol.embed(X, svdX$v)))
 }
@@ -31,10 +40,34 @@ lol.project.pca <- function(X, r, ...) {
 #' @param nu the number of left singular vectors to retain.
 #' @param nv the number of right singular vectors to retain.
 #' @param t the threshold of percent of singular vals/vecs to use irlba.
+#' @param xfm whether to transform the variables before taking the SVD.
+#' \itemize{
+#' \item{FALSE}{apply no transform to the variables.}
+#' \item{'unit'}{unit transform the variables, defaulting to centering and scaling to mean 0, variance 1. See \link[base]{scale} for details and optional args.}
+#' \item{'log'}{log-transform the variables, for use-cases such as having high variance in larger values. Defaults to natural logarithm. See \link[base]{log} for details and optional args.}
+#' \item{'rank'}{rank-transform the variables. Defalts to breaking ties with the average rank of the tied values. See \link[base]{rank} for details and optional args.}
+#' \item{c(opt1, opt2, etc.)}{apply the transform specified in opt1, followed by opt2, etc.}
+#' }
+#' @param xfm.opts optional arguments to pass to the \code{xfm} option specified. Should be a numbered list of lists, where \code{xfm.opts[[i]]} corresponds to the optional arguments for \code{xfm[i]}. Defaults to the default options for each transform scheme.
 #' @return the svd of X.
 #' @author Eric Bridgeford
-lol.utils.svd <- function(X, nu=0, nv=0, t=.05) {
+lol.utils.svd <- function(X, xfm=FALSE, xfm.opts=list(), nu=0, nv=0, t=.05) {
   n <- nrow(X)
+  # scale if desired before taking SVD
+  for (i in 1:length(scale)) {
+    sc <- xfm[i]
+    if (!(i %in% names(scale.opts))) {
+      xfm.opts[[i]] <- list()
+    }
+    if (sc == 'unit') {
+      X <- do.call(scale, c(list(X), xfm.opts[[i]]))
+    } else if (sc == 'log') {
+      X <- do.call(log, c(list(X), xfm.opts[[i]]))
+    } else if (sc == 'rank') {
+      X <- apply(X, c(2), function(x) {do.call(rank, c(list(x), xfm.opts[[i]]))})
+    }
+  }
+  # take svd
   if (nu > t*n | nv > t*n) {
     svdX <- svd(X, nu=nu, nv=nv)
   } else {
@@ -50,6 +83,15 @@ lol.utils.svd <- function(X, nu=0, nv=0, t=.05) {
 #' @param X \code{[n, d]} the data with \code{n} samples in \code{d} dimensions.
 #' @param Y \code{[n]} the labels of the samples with \code{K} unique labels.
 #' @param r the rank of the projection.
+#' @param xfm whether to transform the variables before taking the SVD.
+#' \itemize{
+#' \item{FALSE}{apply no transform to the variables.}
+#' \item{'unit'}{unit transform the variables, defaulting to centering and scaling to mean 0, variance 1. See \link[base]{scale} for details and optional args.}
+#' \item{'log'}{log-transform the variables, for use-cases such as having high variance in larger values. Defaults to natural logarithm. See \link[base]{log} for details and optional args.}
+#' \item{'rank'}{rank-transform the variables. Defalts to breaking ties with the average rank of the tied values. See \link[base]{rank} for details and optional args.}
+#' \item{c(opt1, opt2, etc.)}{apply the transform specified in opt1, followed by opt2, etc.}
+#' }
+#' @param xfm.opts optional arguments to pass to the \code{xfm} option specified. Should be a numbered list of lists, where \code{xfm.opts[[i]]} corresponds to the optional arguments for \code{xfm[i]}. Defaults to the default options for each transform scheme.
 #' @param ... trailing args.
 #' @return A list of class \code{embedding} containing the following:
 #' \item{A}{\code{[d, r]} the projection matrix from \code{d} to \code{r} dimensions.}
@@ -66,7 +108,7 @@ lol.utils.svd <- function(X, nu=0, nv=0, t=.05) {
 #' X <- data$X; Y <- data$Y
 #' model <- lol.project.pca(X=X, Y=Y, r=2)  # use cpca to project into 2 dimensions
 #' @export
-lol.project.cpca <- function(X, Y, r, ...) {
+lol.project.cpca <- function(X, Y, r, xfm=FALSE, xfm.opts=list(), ...) {
   # class data
   classdat <- lol.utils.info(X, Y)
   priors <- classdat$priors; centroids <- t(classdat$centroids)
@@ -77,7 +119,7 @@ lol.project.cpca <- function(X, Y, r, ...) {
   Yidx <- sapply(Y, function(y) which(ylabs == y))
   Xt <- X - centroids[Yidx,]
   # compute the standard projection but with the pre-centered data.
-  svdX <- lol.utils.svd(Xt, nv=r, nu=0)
+  svdX <- lol.utils.svd(Xt, xfm=xfm, xfm.opts=xfm.opts, nv=r, nu=0)
 
   return(list(A=svdX$v, d=svdX$d, centroids=centroids, priors=priors, ylabs=ylabs,
               Xr=lol.embed(X, svdX$v), cr=lol.embed(centroids, svdX$v)))
